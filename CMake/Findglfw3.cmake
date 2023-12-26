ec829d4f246045adee197660fdc97698abab12a9
kuang <snowy-shadow@github.com>
2023-12-26 16:53:25 -0600

    finds moltenvk for macos

--- CMake/Findglfw3.cmake
+++
@@ -1,7 +0,0 @@
-find_path(GLFW_INCLUDE_DIRS GLFW/glfw3.h
-        PATH_SUFFIXES "include"
-        PATHS ${GLFW_PATH})
-
-find_library(GLFW_LIBRARIES NAMES glfw3
-        PATH_SUFFIXES "lib-vc2022"
-        PATHS ${GLFW_PATH})
