cf94e527ca01b7d31ca7bbd0aa52a4e74b85856a
kuang <snowy-shadow@github.com>
2023-12-26 16:53:57 -0600

    New glslang compiler

---
+++ Src/GLSL_Compiler.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Kuang on 2023-12-26.
+//
+
+#include "GLSL_Compiler.h"
+
+namespace VT
+{
+    std::vector<uint32_t> GLSL_Compiler::compile(ShadercFileInfo FileInfo)
+    {
+        auto File = readFile(FileInfo.FileLocation + FileInfo.FileName);
+        std::string Source { File.cbegin(), File.cend() };
+
+        shaderc::SpvCompilationResult Module
+        {
+            m_Compiler.CompileGlslToSpv(
+                Source,
+                FileInfo.ShaderType,
+                FileInfo.FileName.c_str(),
+                FileInfo.CompileOptions)
+        };
+
+        if(Module.GetCompilationStatus() != shaderc_compilation_status_success)
+        { throw std::runtime_error("Failed to compile " + FileInfo.FileLocation + "/" + FileInfo.FileName + " Message : \n" + Module.GetErrorMessage()); }
+
+        return { Module.cbegin(), Module.cend() };
+    }
+
+}
--- src/GLSL_Compiler.h
+++ src/GLSL_Compiler.h
@@ -6,15 +6,37 @@
 
 namespace VT
 {
+    struct ShadercFileInfo : public FileInfo
+    {
+        shaderc_shader_kind ShaderType;
+        shaderc::CompileOptions CompileOptions;
+    };
+
 	class GLSL_Compiler
 	{
 	public:
-		GLSL_Compiler();
-		std::vector<uint32_t> compile(ShaderFileInfo);
+		GLSL_Compiler() = default;
+
+        [[nodiscard]]
+		std::vector<uint32_t> compile(ShadercFileInfo);
 
 		GLSL_Compiler(const GLSL_Compiler&) = delete;
 		GLSL_Compiler& operator = (const GLSL_Compiler&) = delete;
+        ~GLSL_Compiler() = default;
 	private:
-
+        shaderc::Compiler m_Compiler;
 	};
+}
+
+namespace std
+{
+    template<>
+    struct hash<VT::ShadercFileInfo>
+    {
+        size_t operator()(const VT::ShadercFileInfo& F) const noexcept
+        {
+            // can't hash shaderc::CompileOptions
+            return hash<shaderc_shader_kind>()(F.ShaderType) ^ (hash<VT::FileInfo>()(F) << 7);
+        }
+    };
 }
